202f8551aeca7c29ad0c4bf057832113e8fbb688
Jose Castro <drakonc@gmail.com>
2022-02-03 13:39:35 -0500

    Livewire Datatable 1/5 - Instalaciones necesarias y paginación. Finish

--- app/Http/Livewire/LiveUserTable.php
+++ app/Http/Livewire/LiveUserTable.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire;
 
-use Livewire\Component;
+use App\Models\User;
+use Livewire\{Component, WithPagination};
+
 
 class LiveUserTable extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.live-user-table');
+        return view('livewire.live-user-table', ['users' => User::paginate(5)]);
     }
 }
